a9d2d66319bdc9806b866b1ee1e4985b90937abf
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-03-06 17:44:47 -0300

    Comienzo de los proyects

---
+++ licuadoraJS/js/main.js
@@ -0,0 +1,3 @@
+/**Lo primero que hacemos es crear una variable para checkear si la licuadora está encendida o apagada */
+let onOffBlender = 'apagada';
+/**Luego creamos una function que va a corroborar en que estado se encuentra la licuadora y va a alternar su estado */
