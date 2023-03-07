d4dd04a4834b28563f43f0086b6665b396b67ca4
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-03-07 17:35:14 -0300

    completeishon the blender jeje

--- licuadoraJS/js/main.js
+++ licuadoraJS/js/main.js
@@ -1,3 +1,32 @@
+const buttonBlender = document.querySelector('.blender-button');
+const divBlender = document.querySelector('#blender');
+const blenderSound = document.querySelector('#blender-sound');
+const buttonBlenderSound = document.querySelector('#blender-button-sound');
+
 /**Lo primero que hacemos es crear una variable para checkear si la licuadora está encendida o apagada */
 let onOffBlender = 'apagada';
 /**Luego creamos una function que va a corroborar en que estado se encuentra la licuadora y va a alternar su estado */
+buttonBlender.addEventListener('click', blenderControllerStatus)
+const soundsBlender=()=>{
+    if(blenderSound.paused){
+        buttonBlenderSound.play()
+        blenderSound.play();
+    }else{
+        buttonBlenderSound.play()
+        blenderSound.pause();
+        blenderSound.currentTime=0
+    }
+}
+function blenderControllerStatus(){
+    soundsBlender()
+    if (onOffBlender=='apagada'){
+        onOffBlender='encendido'
+        console.log('toy funcionando');
+        divBlender.classList.add('active')
+    }else{
+        
+        onOffBlender='apagada'
+        console.log('i am not funkeishon (yo no funko)');
+        divBlender.classList.remove('active')
+    }
+}
